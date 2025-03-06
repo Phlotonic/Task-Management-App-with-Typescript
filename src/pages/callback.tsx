c9ac3bbfacaeeb0d2619c28df2b70a9bc25c2b97
Adam Dryden <76844656+Phlotonic@users.noreply.github.com>
2025-03-06 15:39:51 -0600

    Added callback page syntax

--- src/pages/callback.tsx
+++ src/pages/callback.tsx
@@ -1,2 +1,15 @@
-const callbackpage: React.FC = () => {
+import { useAuth0 } from "@auth0/auth0-react";
+import { Container, Col } from "react-bootstrap";
+import NavBar from "./NavBar";
+const CallbackPage:React.FC = () =>{
+    const { error, user }= useAuth0();
+    if(error){
+        return <div>Oops...{error.message}</div>
+    }
+    return(
+            <Container>
+                <h1>Welcome{user? `, ${user.given_name}!`:'!'}</h1>
+            </Container>
+    );
 };
+export default CallbackPage;
